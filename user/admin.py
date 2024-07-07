e9c42d383d4646dfbaa251159b1fc4cce9b6628e
MaxymChyncha <max.chyncha@gmail.com>
2024-07-07 11:32:25 +0300

    feat(user): add custom User model & register to admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class ApartmentAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "role", "is_staff")
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (_("Permissions"), {"fields": ("role", "is_active", "is_staff", "is_superuser",
+                                       "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "role", "password1", "password2"),
+        }),
+    )
+    search_fields = ("username", "first_name", "last_name", "email")
+    ordering = ("username",)
+    filter_horizontal = ("groups", "user_permissions",)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class UserRole(models.TextChoices):
+        ADMIN = "admin", _("Admin")
+        MANAGER = "manager", _("Manager")
+        GUARD = "guard", _("Guard")
+
+    role = models.CharField(
+        max_length=10,
+        choices=UserRole,
+        default=UserRole.GUARD
+    )
+
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name} - {self.role}"
