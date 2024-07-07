7d5dd022c0e46cd7061c63e7ccb70feb46858adf
MaxymChyncha <max.chyncha@gmail.com>
2024-07-07 11:48:00 +0300

    feat(building): add Building, Entrance, Apartment models & register to admin panel

--- building/admin.py
+++ building/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from building.models import Building, Entrance, Apartment
 
-# Register your models here.
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ("address", "manager",)
+    list_filter = ("manager",)
+
+
+@admin.register(Entrance)
+class EntranceAdmin(admin.ModelAdmin):
+    list_display = ("number", "building", "guard",)
+    list_filter = ("building", "guard",)
+
+
+@admin.register(Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ("number", "entrance",)
+    list_filter = ("entrance",)
--- building/models.py
+++ building/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Building(models.Model):
+    address = models.CharField(max_length=255)
+    manager = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="buildings",
+    )
+
+    class Meta:
+        permissions = (
+            ("assign_manager", "Assign Manager"),
+        )
+
+    def __str__(self) -> str:
+        return self.address
+
+
+class Entrance(models.Model):
+    building = models.ForeignKey(
+        Building,
+        on_delete=models.CASCADE,
+        related_name="entrances",
+    )
+    number = models.PositiveIntegerField()
+    guard = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="entrances"
+    )
+
+    class Meta:
+        permissions = (
+            ("assign_guard", "Assign Guard"),
+        )
+
+    def __str__(self) -> str:
+        return f"Building: {self.building.address}, Number: {self.number}"
+
+
+class Apartment(models.Model):
+    entrance = models.ForeignKey(
+        Entrance,
+        on_delete=models.CASCADE,
+        related_name="apartments"
+    )
+    number = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return f"Entrance: {self.entrance.number}, Number: {self.number}"
