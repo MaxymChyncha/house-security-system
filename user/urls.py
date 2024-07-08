0373344043d9b789e1cb67b02b4a2150447cbdfc
MaxymChyncha <max.chyncha@gmail.com>
2024-07-08 08:46:10 +0300

    feat(user): add logic for register, login and staff endpoints

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,16 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+from user.views import CreateUserView, ManageUserView
+
+router = routers.DefaultRouter()
+router.register("staff", ManageUserView, basename="staff")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", views.obtain_auth_token, name="login"),
+]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, mixins, viewsets
 
-# Create your views here.
+from building.permissions import IsAdminRole, AllowAnyRole
+from user.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class ManageUserView(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = (IsAdminRole,)
+
+        if self.action == "retrieve":
+            permission_classes = (AllowAnyRole,)
+
+        return [permission() for permission in permission_classes]
+
+    def get_queryset(self):
+        queryset = User.objects.get(pk=self.request.user.pk)
+
+        if self.request.user.role == "admin":
+            queryset = User.objects.all()
+
+        return queryset
