e16d0675cba05dda07195baa84f30ea081214167
MaxymChyncha <max.chyncha@gmail.com>
2024-07-09 09:06:41 +0300

    feat(user/building): add docstring for Swagger documentation, add UserLoginView

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken import views
 
-from user.views import CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView, LoginUserView
 
 router = routers.DefaultRouter()
 router.register("staff", ManageUserView, basename="staff")
@@ -10,7 +10,7 @@ router.register("staff", ManageUserView, basename="staff")
 urlpatterns = [
     path("", include(router.urls)),
     path("register/", CreateUserView.as_view(), name="register"),
-    path("login/", views.obtain_auth_token, name="login"),
+    path("login/", LoginUserView.as_view(), name="login"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics, mixins, viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from building.permissions import IsAdminRole, AllowAnyRole
 from user.serializers import UserSerializer
@@ -8,8 +10,20 @@ User = get_user_model()
 
 
 class CreateUserView(generics.CreateAPIView):
+    """
+    View for creating a new user.
+
+    Allow only for admin roles
+    """
     serializer_class = UserSerializer
     permission_classes = (IsAdminRole,)
+
+
+class LoginUserView(ObtainAuthToken):
+    """
+    View for creating a new auth token.
+    """
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(
@@ -19,6 +33,15 @@ class ManageUserView(
     mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
+    """
+    ViewSet for managing users.
+
+    Depending on role the following CRUD are available:
+
+    - Admin: all CRUD
+    - Manager: GET. Only for related objects
+    - Guard: GET. Only for related objects
+    """
     serializer_class = UserSerializer
 
     def get_permissions(self):
