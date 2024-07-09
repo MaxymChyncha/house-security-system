e2106ebf7f5fee979976f51af5a3fb1dd944a25a
MaxymChyncha <max.chyncha@gmail.com>
2024-07-09 09:04:55 +0300

    feat(config): connect drf-spectacular and django debug toolbar

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("building.urls")),
     path("api/users/", include("user.urls")),
-]
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+] + debug_toolbar_urls()
